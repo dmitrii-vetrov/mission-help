54fc27fc9a0f91c10a2c2300e1e28a12f506095a
Dmitrii Vetrov <24583058+dmitrii-vetrov@users.noreply.github.com>
2021-09-19 14:08:49 +0700

    init modules.
    modules beginning.

---
+++ app/scripts/manager/mainContainer.js
@@ -0,0 +1,39 @@
+
+export default class MainContainer {
+    modules = [];
+
+    constructor() {
+        
+    }
+
+    addModule(module) {
+        console.log(module);
+        if(this.modules.indexOf(module) === -1) {
+            this.modules.push(module);
+        }
+    }
+
+    preload() {
+        this.modules.forEach((module) => {
+            if (module.preload) {
+                module.preload();
+            }
+        })
+    }
+
+    create() {
+        this.modules.forEach((module) => {
+            if (module.create) {
+                module.create();
+            }
+        })
+    }
+
+    update() {
+        this.modules.forEach((module) => {
+            if (module.update) {
+                module.update();
+            }
+        })
+    }
+}
