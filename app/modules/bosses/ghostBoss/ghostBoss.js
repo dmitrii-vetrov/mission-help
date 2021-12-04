5e658f600bd6c7de696d969ef0c723614527fb1a
marfamegtr <86145209+marfamegtr@users.noreply.github.com>
2021-12-04 16:39:59 +0700

    ghostBoss.shoot

--- app/modules/bosses/ghostBoss/ghostBoss.js
+++ app/modules/bosses/ghostBoss/ghostBoss.js
@@ -1,6 +1,7 @@
 class GhostBossModule {
     ghostBoss = null;
     fist = null;
+    isCanShoot = true;
 
     constructor() {
     }
@@ -20,9 +21,30 @@ class GhostBossModule {
 
     update() {
         this.fist.x = this.ghostBoss.x - 70;
-        let playerPoints = new Phaser.Geom.Point(this.x,this.y+this.displayHeight/2);
-        let ghostBossPoints = new Phaser.Geom.Point(this.x,this.y+this.displayHeight/2);
+        // let playerPoints = new Phaser.Geom.Point(this.x,this.y+this.displayHeight/2);
+        // let ghostBossPoints = new Phaser.Geom.Point(this.x,this.y+this.displayHeight/2);
 
        //console.log(phaser.Math.Angle.BetweenPoints(playerPoints, ghostBossPoints))
+
+       if (this.isCanShoot === true) {
+           this.doShoot();
+       }
     }
+    
+    doShoot() {
+        this.isCanShoot = false;
+        phaser.tweens.add({
+            targets: this.fist,
+            y: phaser.game.renderer.height,
+            duration: 5000,
+            repeat: 0,
+            onStart() {
+                
+            },
+            onComplete() {
+
+            },
+          })
+    }
+
 }
